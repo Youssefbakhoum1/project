8081ba37249bdbef4e3d7abac6bd2eb4de0bfdcc
youssef fayek <yousseffayek12345@gmail.com>
2024-07-30 15:16:19 +0300

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4$48@xy#w!2pnqq-^5ml0#ch07m8apc36i$x4w&s0@c@)g7qa3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!mmmmmmmmmmmmmmmm
 DEBUG = True
 
 ALLOWED_HOSTS = []
